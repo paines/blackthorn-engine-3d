6a7270ab2c6a9139dbdf9c08a738c5733ea938d8
askgeek <askgeek@gmail.com>
2011-04-08 16:31:08 -0700

    xbox360 controller

---
+++ Makefile
@@ -0,0 +1,265 @@
+#### Blackthorn -- Lisp Game Engine
+####
+#### Copyright (c) 2007-2011, Elliott Slaughter <elliottslaughter@gmail.com>
+####
+#### Permission is hereby granted, free of charge, to any person
+#### obtaining a copy of this software and associated documentation
+#### files (the "Software"), to deal in the Software without
+#### restriction, including without limitation the rights to use, copy,
+#### modify, merge, publish, distribute, sublicense, and/or sell copies
+#### of the Software, and to permit persons to whom the Software is
+#### furnished to do so, subject to the following conditions:
+####
+#### The above copyright notice and this permission notice shall be
+#### included in all copies or substantial portions of the Software.
+####
+#### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#### EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#### MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#### NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#### HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#### WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#### OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#### DEALINGS IN THE SOFTWARE.
+####
+
+# Search PATH for a Lisp compiler.
+cl := $(shell build/scripts/find-lisp.sh)
+ifeq (${cl},)
+	$(error No Lisp compiler found.)
+endif
+
+# Which ASDF system to load:
+system := blank
+
+# Stardard drivers:
+quicklisp-setup := build/scripts/quicklisp-setup.lisp
+prop :=  build/scripts/property.lisp
+nop := build/scripts/nop.lisp
+load :=  build/scripts/load.lisp
+test :=  build/scripts/test.lisp
+dist :=  build/scripts/dist.lisp
+prof :=  build/scripts/profile.lisp
+atdoc :=  build/scripts/atdoc.lisp
+
+# Select which driver to run (load by default).
+driver := ${load}
+
+args :=
+
+# Specify the run command for the installer.
+ifeq (${cl}, allegro)
+	command := \\\"\\x24INSTDIR\\\\main.exe\\\"
+else
+	command := \\\"\\x24INSTDIR\\\\chp\\\\chp.exe\\\" \\\"\\x24INSTDIR\\\\main.exe\\\"
+endif
+
+# A temporary file for passing values around.
+tempfile := .tmp
+
+# A command which can be used to get an ASDF system property.
+ifeq (${cl}, allegro)
+	get-property = $(shell alisp +B +s ${quicklisp-setup} +s ${prop} -e "(defparameter *driver-system* '|${system}|)" -e "(defparameter *output-file* \"${tempfile}\")" -e "(defparameter *output-expression* '$(1))")
+else
+ifeq (${cl}, sbcl)
+	get-property = $(shell sbcl --load ${quicklisp-setup} --eval "(defparameter *driver-system* \"${system}\")" --eval "(defparameter *output-file* \"${tempfile}\")" --eval "(defparameter *output-expression* '$(1))" --load ${prop})
+else
+ifeq (${cl}, sbcl-builtin)
+	get-property = $(shell SBCL_HOME=$(shell build/scripts/pwd.sh)/build/sbcl/ build/sbcl/sbcl --load ${quicklisp-setup} --eval "(defparameter *driver-system* \"${system}\")" --eval "(defparameter *output-file* \"${tempfile}\")" --eval "(defparameter *output-expression* '$(1))" --load ${prop})
+else
+ifeq (${cl}, clisp)
+	get-property = $(shell clisp -x "(load \"${quicklisp-setup}\")" -x "(defparameter *driver-system* \"${system}\")" -x "(defparameter *output-file* \"${tempfile}\")" -x "(defparameter *output-expression* '$(1))" -x "(load \"${prop}\")")
+else
+ifeq (${cl}, ecl)
+	get-property = $(shell ecl -load ${quicklisp-setup} -eval "(defparameter *driver-system* \"${system}\")" -eval "(defparameter *output-file* \"${tempfile}\")" -eval "(defparameter *output-expression* '$(1))" -load ${prop})
+else
+ifeq (${cl}, clozure)
+	get-property = $(shell ccl --load ${quicklisp-setup} --eval "(defparameter *driver-system* \"${system}\")" --eval "(defparameter *output-file* \"${tempfile}\")" --eval "(defparameter *output-expression* '$(1))" --load ${prop})
+else
+ifeq (${cl}, clozure-builtin)
+	get-property = $(shell build/ccl/wx86cl.exe --load ${quicklisp-setup} --eval "(defparameter *driver-system* \"${system}\")" --eval "(defparameter *output-file* \"${tempfile}\")" --eval "(defparameter *output-expression* '$(1))" --load ${prop})
+endif
+endif
+endif
+endif
+endif
+endif
+endif
+
+# Get ASDF system properties for the specified system.
+define get-properties
+	$(eval temp := $$(call get-property,(asdf:component-name *system*)))
+	$(eval name := $$(shell cat $${tempfile}))
+	$(eval temp := $$(call get-property,(or (asdf:component-property *system* :long-name) (asdf:component-name *system*))))
+	$(eval longname := $$(shell cat $${tempfile}))
+	$(eval temp := $$(call get-property,(asdf:component-version *system*)))
+	$(eval version := $$(shell cat $${tempfile}))
+	$(eval temp := $$(call get-property,(asdf:system-description *system*)))
+	$(eval description := $$(shell cat $${tempfile}))
+	$(eval temp := $$(call get-property,(asdf:component-property *system* :url)))
+	$(eval url := $$(shell cat $${tempfile}))
+endef
+
+export cl, db, system, driver, name, longname, version, description, url, command
+
+.PHONY: new
+new:
+	$(MAKE) clean
+	$(MAKE) load
+
+.PHONY: load
+load:
+	$(MAKE) load-${cl}
+
+.PHONY: load-allegro
+load-allegro:
+	alisp +B +s ${quicklisp-setup} +s ${driver} -e "(defparameter *driver-system* '|${system}|)" -- ${args}
+
+.PHONY: load-sbcl
+load-sbcl:
+	sbcl --eval "(defparameter *driver-system* \"${system}\")" --load ${quicklisp-setup} --load ${driver} -- ${args}
+
+.PHONY: load-sbcl-builtin
+load-sbcl-builtin:
+	SBCL_HOME=$(shell build/scripts/pwd.sh)/build/sbcl/ build/sbcl/sbcl --eval "(defparameter *driver-system* \"${system}\")" --load ${quicklisp-setup} --load ${driver} -- ${args}
+
+.PHONY: load-clisp
+load-clisp:
+	clisp -x "(defparameter *driver-system* \"${system}\")" -x "(load \"${quicklisp-setup}\")" -x "(load \"${driver}\")" -- ${args}
+
+.PHONY: load-ecl
+load-ecl:
+	ecl -eval "(defparameter *driver-system* \"${system}\")" -load ${quicklisp-setup} -load ${driver} -- ${args}
+
+.PHONY: load-clozure
+load-clozure:
+	ccl --eval "(defparameter *driver-system* \"${system}\")" --load ${quicklisp-setup} --load ${driver} -- ${args}
+
+.PHONY: load-clozure-builtin
+load-clozure-builtin:
+	build/ccl/wx86cl.exe --eval "(defparameter *driver-system* \"${system}\")" --load ${quicklisp-setup} --load ${driver} -- ${args}
+
+.PHONY: shell
+shell:
+	$(MAKE) driver="${nop}" new
+
+.PHONY: server
+server:
+	$(MAKE) args="--server=127.0.0.1 --port=12345" new
+
+.PHONY: client
+client:
+	$(MAKE) args="--connect=127.0.0.1 --port=12345" new
+
+.PHONY: server3
+server3:
+	$(MAKE) args="--server --port=12345 --players=3" new
+
+.PHONY: server4
+server4:
+	$(MAKE) args="--server --port=12345 --players=4" new
+
+.PHONY: server5
+server5:
+	$(MAKE) args="--server --port=12345 --players=5" new
+
+.PHONY: slime
+slime:
+	$(MAKE) clean
+	emacs --eval "(progn (slime '${cl}) (while (not (slime-connected-p)) (sleep-for 0 200)) (slime-interactive-eval \"(defparameter *driver-system* \\\"${system}\\\")\") (slime-load-file \"${driver}\"))"
+
+.PHONY: thopter
+thopter:
+	$(MAKE) system="thopter" new
+
+.PHONY: bunny
+bunny:
+	$(MAKE) system="bunnyslayer" new
+
+.PHONY: stress
+stress:
+	$(MAKE) system="blackthorn-stress-test" new
+
+.PHONY: collision
+collision:
+	$(MAKE) system="blackthorn-collision-test" new
+
+.PHONY: test
+test:
+	$(MAKE) driver="${test}" system="blackthorn3d-test" new
+
+.PHONY: prof
+prof:
+	$(MAKE) driver="${prof}" new
+
+.PHONY: distnoclean
+distnoclean:
+	mkdir -p bin
+	cp -r $(wildcard lib/*) disp sound bin
+	$(MAKE) driver="${dist}" new
+
+.PHONY: dist
+dist:
+	$(MAKE) distclean distnoclean
+
+.PHONY: doc
+doc:
+	$(MAKE) docclean
+	$(MAKE) atdoc
+
+.PHONY: atdoc
+atdoc:
+	$(MAKE) driver="${atdoc}" new
+
+.PHONY: install-w32
+install-w32:
+	-$(call get-properties)
+	$(MAKE) distclean
+	mkdir -p bin
+	if test -e windows/${name}.ico; then cp windows/${name}.ico bin/app.ico; else cp windows/bt.ico bin/app.ico; fi
+	$(MAKE) distnoclean postinstall-w32
+
+.PHONY: postinstall-w32
+postinstall-w32:
+	-$(call get-properties)
+	cp -r windows/chp windows/is_user_admin.nsh COPYRIGHT bin
+	awk "{gsub(/@NAME@/, \"${name}\");print}" windows/install.nsi | awk "{gsub(/@LONGNAME@/, \"${longname}\");print}" | awk "{gsub(/@VERSION@/, \"${version}\");print}" | awk "{gsub(/@DESCRIPTION@/, \"${description}\");print}" | awk "{gsub(/@URL@/, \"${url}\");print}" | awk "{gsub(/@COMMAND@/, \"${command}\");print}" > bin/install.nsi
+	makensis bin/install.nsi
+	mv bin/*-install.exe .
+
+.PHONY: install-unix
+install-unix:
+	-$(call get-properties)
+	$(MAKE) dist
+	cp unix/run.sh bin
+	mv bin ${name}
+	tar cfz ${name}-${version}-linux.tar.gz ${name}
+	mv ${name} bin
+
+.PHONY: install-mac
+install-mac:
+	-$(call get-properties)
+	rm -rf "${longname}.app"
+	$(MAKE) dist
+	mkdir "${longname}.app" "${longname}.app/Contents" "${longname}.app/Contents/MacOS" "${longname}.app/Contents/Frameworks" "${longname}.app/Contents/Resources"
+	awk "{gsub(/@NAME@/, \"${name}\");print}" macosx/Info.plist | awk "{gsub(/@LONGNAME@/, \"${longname}\");print}" | awk "{gsub(/@VERSION@/, \"${version}\");print}" | awk "{gsub(/@DESCRIPTION@/, \"${description}\");print}" | awk "{gsub(/@URL@/, \"${url}\");print}" > "${longname}.app/Contents/Info.plist"
+	cp -r $(wildcard lib/*.framework) "${longname}.app/Contents/Frameworks"
+	cp -r disp sound "${longname}.app/Contents/Resources"
+	cp bin/main "${longname}.app/Contents/MacOS"
+	cp macosx/PkgInfo COPYRIGHT "${longname}.app/Contents"
+	if test -e "macosx/${name}.icns"; then cp "macosx/${name}.icns" "${longname}.app/Contents/Resources/app.icns"; else cp macosx/bt.icns "${longname}.app/Contents/Resources/app.icns"; fi
+	tar cfz "${name}-${version}-macos.tar.gz" "${longname}.app"
+	$(MAKE) -f dmg_utils.make NAME="${longname}" VERSION="${version}" SOURCE_DIR=. SOURCE_FILES="${longname}.app COPYRIGHT"
+
+.PHONY: clean
+clean:
+	rm -rf ${tempfile}
+
+.PHONY: docclean
+docclean:
+	rm -rf doc
+
+.PHONY: distclean
+distclean:
+	$(MAKE) clean docclean
+	rm -rf build.in build.out bin
---
+++ src/blackthorn3d/utils/math.lisp
@@ -0,0 +1,376 @@
+;;;; Blackthorn -- Lisp Game Engine
+;;;;
+;;;; Copyright (c) 2011, Robert Gross <r.gross.3@gmail.com>
+;;;;
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation
+;;;; files (the "Software"), to deal in the Software without
+;;;; restriction, including without limitation the rights to use, copy,
+;;;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;;;; of the Software, and to permit persons to whom the Software is
+;;;; furnished to do so, subject to the following conditions:
+;;;;
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;;; DEALINGS IN THE SOFTWARE.
+;;;;
+
+(in-package :blackthorn3d-utils)
+
+;;;;
+;;;; Vector Stuff
+;;;;
+
+(defmacro gen-vec-accessors (&rest names)
+  (labels ((vec-accessor (n p)
+             (with-gensyms (v) `(defmacro ,n (,v) `(svref ,,v ,,p)))))
+    `(progn
+       ,@(iter (for i in names)
+               (for j below (length names))
+               (collect (vec-accessor i j))))))
+        
+;;; Create aliases for accessing different elements
+;;; of vectors
+(gen-vec-accessors x y z w)
+(gen-vec-accessors r g b a)
+
+(defun make-vector4 (x y z w)
+  "@short{Creates a vector of length 4 of floats}
+   @arg[x]{element at index 0}
+   @arg[y]{element at index 1}
+   @arg[z]{element at index 2}
+   @arg[w]{element at index 3}"
+  (make-array 4 :element-type 'float :initial-contents
+    (list x y z w)))
+
+
+(defun make-vector3 (x y z)
+  "@short{Creates a vector of length 3 of floats}
+   @arg[x]{element at index 0}
+   @arg[y]{element at index 1}
+   @arg[z]{element at index 2}"
+  (make-array 3 :element-type 'float :initial-contents
+    (list x y z)))
+
+(defun make-point3 (x y z)
+  "@short{Create a point in 3d space; automatically sets w to 1.0}"
+  (make-vector4 x y z 1.0))
+
+(defun make-vec3 (x y z)
+  "@short{Create a vector in 3d space; sets w to 0.0}"
+  (make-vector4 x y z 0.0))
+
+(defun dot (a b)
+  (iter (for i from 0 below 3)
+        (sum (* (aref a i) (aref b i)))))
+
+(defun cross (a b)
+  (make-vector4
+    (- (* (y a) (z b)) (* (z a) (y b)))
+    (- (* (z a) (x b)) (* (x a) (z b)))
+    (- (* (x a) (y b)) (* (y a) (x b)))
+    (w a)))
+
+(defun cross3 (a b)
+  (make-vector3
+    (- (* (y a) (z b)) (* (z a) (y b)))
+    (- (* (z a) (x b)) (* (x a) (z b)))
+    (- (* (x a) (y b)) (* (y a) (x b)))))
+
+(defmacro vector-elt-wise (fn a b)
+  `(map 'vector ,fn ,a ,b))
+
+(defun vec+ (a b)
+  (vector-elt-wise #'- a b))
+  
+(defun vec- (a b)
+  (vector-elt-wise #'- a b))
+
+(defun vec* (a b)
+  (vector-elt-wise #'* a b))
+
+(defun vec/ (a b)
+  (vector-elt-wise #'/ a b))
+
+(defun vec-scale (v s)
+  (make-vector4 
+    (* s (x v))
+    (* s (y v))
+    (* s (z v))
+    (w v)))
+
+(defun vec-scale3 (v s)
+  (make-vector3
+    (* s (x v))
+    (* s (y v))
+    (* s (z v))))
+
+(defun vec-neg (v)
+  (make-vector4 
+    (- (x v))
+    (- (y v))
+    (- (z v))
+    (w v)))
+
+(defun sq (x) (* x x))
+
+(defun mag (v)
+  (sqrt (+ (+ (sq (x v))
+              (sq (y v)))
+           (sq (z v)))))
+
+(defun norm (v)
+  "Normalize a vector4, see normalize for general normalize fn"
+  (let ((magv (mag v)))
+    (unless (zerop magv) (vec-scale v (/ magv)))))
+
+
+(defun normalize (v)
+  "Normalize a vector of any length"
+  (let ((magv (magnitude v)))
+    (unless (zerop magv)
+      (map 'vector #'(lambda (x) (/ x magv)) v))))
+
+(defun homogenize (v)
+  (unless (zerop (w v)) (vec-scale v (/ (w v)))))
+
+(defun min-axis (v)
+  (iter (for i below 4)
+        (finding i minimizing (svref v i))))
+
+(defun make-perp (v)
+  (let ((min-a (min-axis v)))
+    (case min-a
+      (0 (norm (make-vec3 0.0 (-(z v)) (y v))))
+      (1 (norm (make-vec3 (-(z v)) 0.0 (x v))))
+      (2 (norm (make-vec3 (-(y v)) (x v) 0.0))))))
+
+;;;
+;;; Matrices
+;;;
+
+
+(defun transpose-lists (lsts)
+  (apply #'mapcar #'list lsts))
+
+(defun make-matrix (size &optional init-contents)
+  (if init-contents
+    (make-array size :element-type 'float
+                     :initial-contents init-contents)
+    (make-array size :element-type 'float
+                     :initial-element 0.0)))
+
+(defun make-matrix4x4 (&optional init-contents) 
+  (make-matrix '(4 4) init-contents))
+
+(defun make-matrix3x3 (&optional init-contents)
+  (make-matrix '(3 3) init-contents))
+  
+(defun get-nrows (mat)
+  (array-dimension mat 1))
+  
+(defun get-ncols (mat)
+  (array-dimension mat 0))
+
+(defun copy-matrix (mat)
+  "@return{a matrix with the same elements as mat}"
+  (let* ((n-row (get-nrows mat))
+         (n-col (get-ncols mat))
+         (copy  (make-matrix (list n-row n-col))))
+    (iter (for i below (* n-row n-col))
+         (setf (row-major-aref copy i) (row-major-aref mat i)))))
+
+(defun get-col (mat col)
+  "@arg[mat]{matrix of any size}
+   @arg[col]{zero-index column into mat}
+   @return{the column of mat as a simple-vector}"
+  (let ((nrows (get-nrows mat)))
+    (make-array nrows :element-type 'float :initial-contents
+      (iter (for i below nrows)
+            (collect (aref mat col i))))))
+
+(defun set-col (mat col vec)
+  "@arg[mat]{matrix of any size mxn}
+   @arg[col]{zero-index column into mat}
+   @arg[vec]{the vector of length l to replace col. 
+             If l < m. the elements in range [l m) in mat not modified.
+             If l > m, only the [0 m) elements of vec are copied}
+   @return{the modified matrix.  Modifies argument mat}"
+  (let ((nrows (get-nrows mat)))
+    (iter (for i below (min (length vec) nrows))
+          (setf (aref mat col i) (svref vec i)))
+    mat))
+
+(defun get-row (mat row)
+  "@arg[mat]{matrix of any size}
+   @arg[row]{zero-index row into mat}
+   @return{the row of mat as a simple-vector}"
+  (let ((ncols (get-ncols mat)))
+    (make-array ncols :element-type 'float :initial-contents
+      (iter (for i below ncols)
+                (collect (aref mat i row))))))
+
+(defun set-row (mat row vec)
+  "@arg[mat]{matrix of any size mxn}
+   @arg[row]{zero-index row into mat}
+   @arg[vec]{the vector of length l to replace row of mat. 
+             If l < n. the elements in range [l n) in mat not modified.
+             If l > n, only the [0 n) elements of vec are copied}
+   @return{the modified matrix.  Modifies argument mat}"
+  (let ((ncols (get-ncols mat)))
+    (iter (for col below (min (length vec) ncols))
+          (setf (aref mat col row) (svref vec col)))
+    mat))
+
+;;;
+;;; General Matrix and Vector Math
+;;;
+
+(defun magnitude (v)
+  "@short{the magnitude of a vector of any length}"
+  (sqrt (iter (for i in-vector v) (sum (sq i)))))
+
+(defun inner-product (a b)
+  "Computes the inner product of two vectors
+   @returns{float result of a . b}"
+  (let ((len (min (length a) (length b))))
+      (iter (for i below len)
+            (sum (* (aref a i) (aref b i))))))
+
+(defun outer-product (a b)
+  "Computes the outer product of two vectors (of the same size)
+   defined as:
+   a (x) b'
+   @return{a matrix of size (len(a), len(b))}"
+  (let* ((len (min (length a) (length b)))
+         (new-mat (make-matrix (list len len))))
+    (iter (for i below len)
+          (iter (for j below len)
+                (setf (aref new-mat i j) (* (aref a j) (aref b i)))))))
+
+;; Calculate the transpose of a matrix m
+(defun transpose (m)
+  "@return{the transpose of matrix m}"
+  (let* ((n-rows (get-nrows m))
+         (n-cols (get-ncols m))
+         (new-mat (make-matrix (list n-cols n-rows))))
+    (iter (for i below n-rows)
+          (iter (for j below n-cols)
+                (setf (aref new-mat j i) (aref m i j))))
+    new-mat))
+  
+(defun matrix-multiply-v (mat vec)
+  "Multiply matrix mat by column vector vec
+   @arg[mat]{a matrix of size m x n}
+   @arg[vec]{a vector of size n}
+   @return{the result of multiplying mat by vec, a vector of size m}"
+  (let* ((nrows (min (length vec) (get-nrows mat)))
+         (ncols (get-ncols mat))
+         (new-vec (make-array nrows :element-type 'float)))
+    (iter (for i below nrows)
+          (setf (aref new-vec i)
+                (inner-product (get-row mat i) vec)))
+    new-vec))
+
+;; Not sure what we would need this for...
+;; row vectors have little use
+(defun vector-multiply-m (v A)
+  "Multiply a row vector by a matrix
+   @arg[v]{a vector of size m}
+   @arg[A]{a matrix of size m x n}
+   @return{the result of multiplying v by A, a row vector of size n}"
+  (let* ((n-rows (get-nrows A))
+         (n-cols (min (length v) (get-ncols A)))
+         (new-cvec (make-array `(1 ,n-rows) :element-type 'float)))
+    (iter (for i below n-cols)
+          (setf (aref new-cvec 1 i) 
+                (inner-product v (get-col A i))))))
+
+(defun matrix-multiply-m (A B)
+  "Multiply a matrix A by matrix B
+   @arg[A]{Matrix on left-side of multiplication. Should be size m x n}
+   @arg[B]{Matrix on right-side of multiplication.  Should be size n x k}
+   @return{the m x k matrix that results from performing A * B}"
+  (let* ((n-rows (get-nrows A))
+         (n-cols (get-ncols B))
+         (new-mat (make-matrix (list n-rows n-cols))))
+    (iter (for i below n-cols)
+          (iter (for j below n-rows)
+                (setf (aref new-mat j i)
+                      (inner-product (get-row A j)
+                                     (get-col B i)))))))
+
+(defun place-one (index len)
+  (iter (for i below len)
+        (collect (if (= i index) 1.0 0.0))))
+
+(defun make-identity (&optional (size 4))
+  "@return{a square identity matrix}"
+  (make-matrix (list size size)
+               (iter (for i below size)
+                     (collect (place-one i size)))))
+
+;(defun make-ortho-basis (u v w)
+;  "@arg[u]{the axis the x-axis will map to}
+;   @arg[v]{the axis the y-axis will map to}
+;   @arg[z]{the axis the z-axis will map to}
+;   @return{a 4x4 matrix that will convert coordinates from the default
+;           basis to the one defined by u v w}"
+;  (let* ((basis (make-matrix4x4))
+;         (z (norm (vec-neg d)))
+;         (x (norm (cross up z)))
+;         (y (cross z x)))
+;    (set-col cam 0 x) 
+;    (set-col cam 1 y) 
+;    (set-col cam 2 z) 
+;    (set-col cam 3 e)
+;    (set-row cam 3 (make-vector4 0.0 0.0 0.0 1.0))))
+
+(defun make-translate (v)
+  "@return{a translation matrix that will translate points by v}"
+  (set-col (make-identity) 3 v))
+
+(defun make-scale (v)
+  "@return{a scaling matrix that will scale points by v}"
+  (matrix-multiply-v (make-identity) v))
+
+(defun make-x-rot (r)
+  "@arg[r]{radians to rotate around x-axis (counter-clockwise)}
+   @return{a 4x4 rotation matrix}"
+  (let ((cosr (cos r))
+        (sinr (sin r)))
+    (make-matrix4x4 
+      (transpose-lists  `((1.0 0.0 0.0 0.0)
+                          (0.0 ,cosr ,(- sinr) 0.0)
+                          (0.0 ,sinr ,cosr 0.0)
+                          (0.0 0.0 0.0 1.0))))))
+
+(defun make-y-rot (r)
+  "@arg[r]{radians to rotate around y-axis (counter-clockwise)}
+   @return{a 4x4 rotation matrix}"
+  (let ((cosr (cos r))
+        (sinr (sin r)))
+    (make-matrix4x4 
+      (transpose-lists `((,cosr 0.0 ,sinr 0.0)
+                         (0.0 1.0 0.0 0.0)
+                         (,(- sinr) 0.0 ,cosr 0.0)
+                         (0.0 0.0 0.0 1.0))))))
+
+(defun make-z-rot (r)
+  "@arg[r]{radians to rotate around z-axis (counter-clockwise)}
+   @return{a 4x4 rotation matrix}"
+  (let ((cosr (cos r))
+        (sinr (sin r)))
+    (make-matrix4x4 
+      (transpose-lists `((,cosr ,(- sinr) 0.0 0.0)
+                         (,sinr ,cosr 0.0 0.0)
+                         (0.0 0.0 1.0 0.0)
+                         (0.0 0.0 0.0 1.0))))))

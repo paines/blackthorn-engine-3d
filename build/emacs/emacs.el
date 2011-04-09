de9a2ceac171b066f0fd8b6eae936b0a895d401e
Elliott Slaughter <elliottslaughter@gmail.com>
2011-04-09 16:16:00 -0700

    Fixed Emacs paren highlighting.

--- build/emacs/emacs.el
+++ build/emacs/emacs.el
@@ -23,9 +23,21 @@
 ;;;; DEALINGS IN THE SOFTWARE.
 ;;;;
 
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(show-paren-mode t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+
 (setq inhibit-startup-message t)
 (setq-default indent-tabs-mode nil)
-(setq-default show-paren-mode t)
 (delete-selection-mode t)
 
 ;;;

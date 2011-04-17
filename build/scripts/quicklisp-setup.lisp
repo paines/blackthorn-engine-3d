a5c013c42d3c69ee2b97a829ad1bd099a8dcc414
Elliott Slaughter <elliottslaughter@gmail.com>
2011-04-17 13:59:30 -0700

    Added userial library.

--- build/scripts/quicklisp-setup.lisp
+++ build/scripts/quicklisp-setup.lisp
@@ -36,7 +36,18 @@
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
 
-;; Inject library paths for cffi
+;; Add any directories in build/libs to the registry
+(dolist (dir (directory
+              (merge-pathnames
+               "../libs/*/"
+               (make-pathname
+                :host (pathname-host #.(or *compile-file-truename*
+                                           *load-truename*))
+                :directory (pathname-directory #.(or *compile-file-truename*
+                                                     *load-truename*))))))
+  (pushnew dir asdf:*central-registry* :test #'equal))
+
+;; Inject native library paths for cffi
 (ql:quickload :cffi)
 
 (pushnew

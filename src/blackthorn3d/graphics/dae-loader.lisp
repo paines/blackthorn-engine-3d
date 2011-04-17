c6b0a9da481a4355d25f3ea03729945babe07d27
Robert Gross <r.gross.3@gmail.com>
2011-04-17 14:09:24 -0700

    dae-loader operational

---
+++ src/blackthorn3d/graphics/dae-loader.lisp
@@ -0,0 +1,213 @@
+;;;; Blackthorn -- Lisp Game Engine
+;;;;
+;;;; Copyright (c) 2011, Robert Gross <r.gross.3@gmail.com>
+;;;;
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation
+;;;; files (the "Software"), to deal in the Software without
+;;;; restriction, including without limitation the rights to use, copy,
+;;;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;;;; of the Software, and to permit persons to whom the Software is
+;;;; furnished to do so, subject to the following conditions:
+;;;;
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;;; DEALINGS IN THE SOFTWARE.
+;;;;
+
+(in-package :blackthorn3d-graphics)
+
+(defvar +library_geometries+ "library_geometries")
+(defvar +geometry-block+ "geometry")
+(defvar +mesh-block+ "mesh")
+(defvar +source-block+ "source")
+(defvar +accessor+ "accessor")
+(defvar +vertices+ "vertices")
+(defvar +triangles+ "triangles")
+
+(defparameter *file* nil)
+(defparameter *source-ht* nil)
+
+(defun xml-listp (lst)
+  (and (consp lst)
+       (consp (car lst))))
+
+(defun children (xml-lst)
+  (cddr xml-lst))
+
+;; Returns first xml child of xml-lst
+(defun first-child (xml-lst)
+  (find-if #'consp (children xml-lst)))
+
+(defun attributes (xml-lst)
+  (second xml-lst))
+
+(defun tag-name (xml-lst)
+  (caar xml-lst))
+
+(defun find-tag (tag lst) 
+  (if (consp lst)
+      (let ((xml-lst (find-if #'consp lst)))
+        (if (string-equal (tag-name xml-lst) tag)
+            xml-lst
+            (or (find-tag tag (children xml-lst))
+                (find-tag tag (cdr lst)))))
+      nil))
+
+(defun find-tag-in-children (tag xml-lst)
+  (let ((children (remove-if-not #'consp (children xml-lst))))
+    (find tag children
+          :test #'string-equal
+          :key #'car)))
+
+(defun get-attribute (attrib attrib-lst)
+  (aif (member attrib attrib-lst :test #'string-equal :key #'car)
+       (second (car it))
+       nil))
+  
+(defun string->sv (str)
+  (with-input-from-string (s str)
+    (apply #'vector
+           (iter (for val = (read s nil :eof ))
+                 (until (eql val :eof))
+                 (collect val)))))
+
+(defun make-accessor (accessor-lst)
+  (let ((stride (parse-integer (get-attribute "stride" 
+                                              (attributes accessor-lst)))))
+    #'(lambda (array index)
+        (apply #'vector 
+               (iter (for i below stride)
+                     (collect (svref array (+ i (* index stride)))))))))
+
+(defun make-components (accessor-lst)
+  (mapcar #'(lambda (child) 
+              (get-attribute "name" (attributes child)))
+          (remove-if-not #'consp (children accessor-lst))))
+
+(defclass dae-source ()
+  ((id 
+    :accessor src-id
+    :initarg :id)
+   (array 
+    :accessor src-array
+    :initarg :array)
+   (accessor 
+    :accessor src-accessor
+    :initarg :accessor
+    :documentation "function taking an array and index")
+   (components 
+    :accessor src-components
+    :initarg :components)))
+
+(defun make-source (src-lst)
+  (let ((accessor-lst (find-tag +accessor+ (children src-lst))))
+    (make-instance 'dae-source 
+                   :id (get-attribute "id" (attributes src-lst))
+                   :array (string->sv (car (children (first-child src-lst))))
+                   :accessor (make-accessor accessor-lst)
+                   :components (make-components accessor-lst))))
+
+(defun hash-sources (xml-lsts)
+  (let ((src-lst (mapcar #'make-source
+                         (remove-if-not #'(lambda (x)
+                                            (and (consp x)
+                                                 (string-equal (tag-name x) "source")))
+                                        xml-lsts))))
+    (iter (for src in src-lst)
+          (setf (gethash (src-id src) *source-ht*) src))))
+
+(defun set-vertices (vert-lst)
+  (setf (gethash (get-attribute "id" (attributes vert-lst)) *source-ht*)
+        (gethash (subseq (get-attribute "source" (attributes (first-child vert-lst))) 1) 
+                 *source-ht*)))
+
+(defun input->source (str)
+  (gethash (subseq str 1) *source-ht*))
+
+(defun build-input-lst (prim-chld-lst)
+  (iter (for xml in prim-chld-lst)
+        (if (and (consp xml) (string-equal "input" (tag-name xml)))
+            (let ((attribs (attributes xml)))
+              (collect (list (get-attribute "semantic" attribs)
+                             (input->source (get-attribute "source" attribs))))))))
+
+
+(gl:define-gl-array-format blt-mesh
+  (gl:vertex :type :float :components (px py pz))
+  (gl:normal :type :float :components (nx ny nz))
+  (gl:tex-coord :type :float :components (u v)))
+
+
+;; Returns a list of (fn . array) where calling fn with an index modifies array
+;; fn is designed to take in index to a source and add the corresponding value(s) 
+;; to array. 
+(defun get-source-functions (inputs)
+  (iter (for i in inputs)
+        (collect 
+         (let* ((source (second i))
+                (attrib-len (length (src-array source)))
+                (attrib-vec (make-array attrib-len :fill-pointer 0)))
+           (cons #'(lambda (index)
+                     (let ((src-array-vec (funcall (src-accessor source) 
+                                                   (src-array source) 
+                                                   index)))
+                       (iter (for elt in-vector src-array-vec)
+                             (vector-push elt attrib-vec))))
+                 attrib-vec)))))
+
+;; this function is going to need some serious refactoring ... >_<
+(defun process-indices (tri-lst)
+  (let* ((input-lst (build-input-lst (children tri-lst)))
+         (ind-len (length input-lst))
+         ;; The list of indices
+         (prim-arr (string->sv (third (find-tag "p" (children tri-lst)))))
+         ;; Hash table of previously-seen vertex arrangements
+         (vertex-ht (make-hash-table :test #'equalp))
+         (num-tri (parse-integer (get-attribute "count" (attributes tri-lst))))
+         (num-vert (* num-tri 3))
+         (fns (get-source-functions input-lst))
+         ;; Index Array
+         (curr-index 0)
+         (indices (make-array num-vert :fill-pointer 0)))
+    ;; For each index section: If it's already been seen before get that index
+    ;; otherwise create a new index and set the arrays
+   (iter (for i below (length prim-arr) by ind-len)
+          (let ((vertex (subseq prim-arr i (+ i ind-len))))
+            (aif (gethash vertex vertex-ht)
+                (vector-push it indices)
+                (progn
+                  (setf (gethash vertex vertex-ht) curr-index)
+                  (vector-push curr-index indices)
+                  (iter (for f in fns)
+                        (for i in-vector vertex)
+                        (funcall (car f) i))
+                  (incf curr-index)))))
+    ;; we return a list of each attribute array with it's semantic
+   (mapcar #'(lambda (input fn)
+               (cons (car input) (cdr fn)))
+           input-lst fns)
+    #+disabled(iter (for i in input-lst)
+          (for f in fns)
+          (collect (list (car i) (cdr f))))))
+
+;; constructs a mesh object from an xml-list mesh tag
+(defun build-mesh (xml-lst)
+  (let ((children (children xml-lst)))
+    (setf *source-ht* (make-hash-table :test #'equal))
+    (hash-sources children)
+    (set-vertices (find-tag +vertices+ children))
+    (process-indices (find-tag +triangles+ children))
+    ))
+
+(defun load-dae (filename)
+  (let ((dae-file (cxml:parse-file filename (cxml-xmls:make-xmls-builder))))
+    (build-mesh (find-tag "mesh" (list dae-file)))))
---
+++ src/blackthorn3d/main/main.lisp
@@ -0,0 +1,262 @@
+;;;; Blackthorn -- Lisp Game Engine
+;;;;
+;;;; Copyright (c) 2007-2011, Elliott Slaughter <elliottslaughter@gmail.com>
+;;;;
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation
+;;;; files (the "Software"), to deal in the Software without
+;;;; restriction, including without limitation the rights to use, copy,
+;;;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;;;; of the Software, and to permit persons to whom the Software is
+;;;; furnished to do so, subject to the following conditions:
+;;;;
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;;; DEALINGS IN THE SOFTWARE.
+;;;;
+
+(in-package :blackthorn3d-main)
+
+;;;
+;;; System paths
+;;;
+
+(defun setup-paths ()
+  (add-resource-path *default-pathname-defaults*)
+  (add-resource-path
+   (merge-pathnames #p"../../" #.(or *compile-file-truename* *load-truename*)))
+  #+darwin
+  (add-resource-path
+   (merge-pathnames #p"../Resources/" (command-line-executable))))
+
+;;;
+;;; Command-line option parsing
+;;;
+
+(defun command-line-executable ()
+  "Returns the path to the executable being run."
+  #+allegro (car (sys:command-line-arguments))
+  #+clisp (aref (ext:argv) 0)
+  #+clozure (car ccl:*command-line-argument-list*)
+  #+ecl (car (ext:command-args))
+  #+lispworks (car system:*line-arguments-list*)
+  #+sbcl (car sb-ext:*posix-argv*)
+  #-(or allegro clisp clozure ecl lispworks sbcl)
+  (error "Don't know how to get command line args."))
+
+(defparameter *cli-options*
+  '((("server" #\s) :type string :optional t)
+    (("connect" #\c) :type string :optional t)
+    (("port" #\P) :type integer :initial-value 12345)
+    (("players") :type integer :initial-value 2)))
+
+(defun cli-get-mode ()
+  (let* ((args (command-line-arguments:get-command-line-arguments))
+         (opts (command-line-arguments:process-command-line-options
+                        *cli-options*
+                        (aif (position "--" args :test #'equal)
+                             (nthcdr (1+ it) args)))))
+    (append (or (aif (getf opts :server) (list :server it))
+                (aif (getf opts :connect) (list :client it))
+                (list :normal nil))
+            (list (getf opts :port)
+                  (getf opts :players)))))
+
+;;;
+;;; Main Game Driver
+;;;
+
+; TODO: Move these to the proper package
+(defun deg->rad (x)
+    (* x (/ pi 180)))
+    
+(defclass input-system ()
+    ((kind 
+        :accessor input-kind
+        :initarg :kind
+        :documentation "Either :keyboard or :xbox")))
+        
+(defmethod input-move-x ((system input-system))
+    (with-slots (kind) system
+        (case kind
+            (:keyboard 
+              (+ (if (sdl:get-key-state :sdl-key-right) 1.0  0)
+                 (if (sdl:get-key-state :sdl-key-left) -1.0  0)))
+            #+windows
+            (:xbox (/ (xbox360_get_lx 0) 65535))
+            (otherwise 0))))
+
+(defmethod input-move-y ((system input-system))
+    (with-slots (kind) system
+        (case kind
+            (:keyboard
+               (+ (if (sdl:get-key-state :sdl-key-up) 1.0 0)
+                  (if (sdl:get-key-state :sdl-key-down) -1.0 0)))
+            #+windows
+            (:xbox (/ (xbox360_get_ly 0) 65535))
+            (otherwise 0))))
+        
+(defparameter *input* (make-instance 'input-system :kind :keyboard))
+
+(defmethod set-controller ((system input-system) type)
+    (setf (input-kind system) type))
+
+; end todo
+    
+(defun main-init-abort-handler ()
+  (throw 'main-init nil))
+
+(defun main-loop-abort-handler ()
+  (throw 'main-loop nil))
+
+(defun main-process-event (event)
+  (send *game* event))
+
+(defun main (&key (exit-when-done t))
+  "Main entry point for the game. Deals with initialization, finalization, and the main game loop."
+  ;; Initialization:
+  (setup-paths)
+  (load-dlls)
+
+  ;(unless *game* (error "No game specified.~%"))
+
+  ;(apply #'net-init (cli-get-mode))
+
+  (setf mt19937:*random-state* (mt19937:make-random-state t))
+
+  (unwind-protect
+  (catch 'main-init
+    ;(net-game-connect #'main-init-abort-handler)
+
+    (sdl:with-init ()
+      ;(init-mixer)
+      ;(game-init *game* :player (hostname) :players (hostnames))
+
+      ; TODO: temporary code, abstract this away
+      (sdl:window 800 600 :bpp 32 :flags sdl:sdl-opengl
+                  :title-caption "Test" :icon-caption "Test")
+      (gl:viewport 0 0 800 600)
+
+      (gl:enable :texture-2d)
+      (gl:enable :blend)
+      (gl:blend-func :src-alpha :one-minus-src-alpha)
+      (gl:clear-color 0 0 0 0)
+      (gl:enable :depth-test)
+      (gl:depth-func :lequal)
+      (gl:matrix-mode :projection)
+      (gl:load-identity)
+      (let ((fx 1.0) (fy (* 1.0 6/8)))
+        (gl:frustum (- fx) fx (- fy) fy 1.0 100.0))
+      (gl:matrix-mode :modelview)
+      (gl:load-identity)
+      (gl:light :light0 :position '(3.0 3.0 0.0 1.0))
+      (gl:light :light0 :diffuse (make-vec3 1.0 1.0 1.0))
+      ;(gl:enable :lighting)
+      (gl:enable :light0)
+
+      (defparameter cube (make-cube))
+      ;(gfx-init)
+      (defparameter turn (make-y-rot (/ pi 100)))
+      
+      ;; Main loop:
+      (let ((input-queue (make-instance 'containers:basic-queue))
+            (cam (make-instance 'camera 
+                                :position (make-point3 0.0 0.0 5.0)
+                                :direction (norm4 (vec4- (make-vec3 0.0 0.0 0.0)
+                                                         (make-vec3 0.0 0.0 5.0)))
+                                :mode :third-person ))
+            (cam-quat (axis-rad->quat (make-vec3 0.0 1.0 0.0) (/ pi 100))))
+        ;(camera-orbit! cam 0.0 -0.2 5.0)
+        (catch 'main-loop
+                                        ;(net-game-start #'main-loop-abort-handler)
+
+          (sdl:with-events ()
+            (:quit-event ()
+                                        ;(net-game-quit)
+                         t)
+            (:key-down-event (:key k :mod m :mod-key m-k :unicode u)
+                (when (sdl:key= k :sdl-key-return)
+                  (if (eql (input-kind *input*) :keyboard)
+                    (set-controller *input* :xbox)
+                    (set-controller *input* :keyboard)))
+                                        ;(containers:enqueue
+                                        ; input-queue
+                                        ; (make-instance 'key-event :host (hostname) :type :key-down :key k
+                                        ;                :mod m :mod-key m-k :unicode u))
+                             )
+            (:key-up-event (:key k :mod m :mod-key m-k :unicode u)
+                                        ;(containers:enqueue
+                                        ; input-queue
+                                        ; (make-instance 'key-event :host (hostname) :type :key-up :key k
+                                        ;                :mod m :mod-key m-k :unicode u))
+                           )
+            (:idle ()
+                   
+                   (progn
+                   #+windows
+                     (xbox360_poll 0)
+                                                     
+                     ; (/ (xbox360_get_lx 0) 65535)
+                                                     
+                     ;(setf (cam-dir cam) (quat-rotate-vec cam-quat (cam-dir cam)))
+                     (let ((rot-amt  (* -1 (input-move-x *input*)))
+                           (step-amt (*  1 (input-move-y *input*))))
+                         
+                         (setf (cam-dir cam) (quat-rotate-vec
+                            (axis-rad->quat (make-vec3 0.0 1.0 0.0) (deg->rad (* 2.7 rot-amt)))
+                            (cam-dir cam)))
+                         (setf (cam-pos cam) (vec4+ (cam-pos cam) (vec-scale4 (cam-dir cam) step-amt)) )
+                         )
+                    
+                    
+                    
+                     #+disabled
+                     (let ((x (* 2 (abs (xbox360_get_lx 0))))
+                           (y (* 2 (abs (xbox360_get_ly 0))))) 
+                       (xbox360-vibrate 0 x y)))
+                   
+                   ;; Rotate the camera around the target each frame
+                   #+disabled
+                   (camera-orbit! cam (/ pi 100) 0.0 5.0)
+
+                   (gl:clear :color-buffer-bit :depth-buffer-bit)
+                   (gl:load-matrix (camera-inverse cam))
+                                        ;(gl:translate 1.0 0.0 -1.0)
+                   (gl:color 1.0 .75 0.0)
+                   (apply #'draw-vert-array cube)
+                   ;(gfx-draw)
+                          
+                                        ;(render *game* #c(0 0) 1d0 -1d0)
+                   (gl:flush)
+                   (sdl:update-display)
+
+                   #+blt-debug
+                   (let ((connection (or swank::*emacs-connection*
+                                         (swank::default-connection))))
+                     (when (and connection
+                                (not (eql swank:*communication-style* :spawn)))
+                       (swank::handle-requests connection t)))
+
+                                        ;(net-game-update input-queue #'main-process-event
+                                        ;                 #'main-loop-abort-handler)
+                                        ;(game-update *game*)
+                   )))))
+    ;#-clozure ;; FIXME: This causes a crash on Clozure builds on Windows.
+    ;(unload-graphics)
+    ;(unload-mixer)
+    )
+
+  ;; Finalization:
+  ;(net-exit)
+  #+windows (xbox360-vibrate 0 0 0)
+  )
+  (when exit-when-done
+    (exit)))

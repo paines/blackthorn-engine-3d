ab0bac580eefa0817fcbc07f19b58f49c955fd0c
Elliott Slaughter <elliottslaughter@gmail.com>
2011-04-28 21:02:29 -0700

    Cosmetic.

--- src/blackthorn3d/main/client.lisp
+++ src/blackthorn3d/main/client.lisp
@@ -30,81 +30,83 @@
 (defun handle-message-client (src b size)
   (let ((msg (read-string b)))
     (format t "Msg from ~a was ~a~%" src msg)))
-        
+
 (defvar counter 0)
-        
+
 (defun client-main ()
-    (setup-paths)
-    (load-dlls)
-    (blt3d-gfx:init)
-    
-    (setf *my-client-buffer* (userial:make-buffer))
-    (setf *random-state* (make-random-state t))
+  (setup-paths)
+  (load-dlls)
+  (blt3d-gfx:init)
+
+  (setf *my-client-buffer* (userial:make-buffer))
+  (setf *random-state* (make-random-state t))
+
+  ;; TODO: Don't hard code connection information
+  (handler-case 
+      (socket-client-connect "127.0.0.1" 9001 :timeout 1.0)
+    (usocket:connection-refused-error ()
+      ;; TODO: handle this more gracefully.
+      (format t "Error: Connection refused.~%")
+      (return-from client-main)))
         
-    ; TODO: Don't hard code connection information
-    (handler-case 
-        (socket-client-connect "127.0.0.1" 9001 :timeout 1.0)
-      (usocket:connection-refused-error ()
-        ; TODO: handle this more gracefully.
-        (format t "Error: Connection refused.~%")
-        (return-from client-main)))
+  (setf mt19937:*random-state* (mt19937:make-random-state t))
         
-    (setf mt19937:*random-state* (mt19937:make-random-state t))
+  (unwind-protect
+       (catch 'main-init
+         (sdl:with-init ()
+           (sdl:window 800 600 :bpp 32 :flags sdl:sdl-opengl
+                       :title-caption "Test" :icon-caption "Test")
+           (blt3d-gfx:prepare-scene)
         
-    (unwind-protect
-    (catch 'main-init
-      (sdl:with-init ()
-        (sdl:window 800 600 :bpp 32 :flags sdl:sdl-opengl
-                  :title-caption "Test" :icon-caption "Test")
-        (blt3d-gfx:prepare-scene)
-        
-        (let ((input-queue (make-instance 'containers:basic-queue)))
-        (catch 'main-loop
-          (sdl:with-events ()
-            (:quit-event () t)
-            (:key-down-event (:key k :mod m :mod-key m-k :unicode u)
-                (when (sdl:key= k :sdl-key-return)
-                  (if (eql (input-kind *input*) :keyboard)
-                    (set-controller *input* :xbox)
-                    (set-controller *input* :keyboard)))
-                                        ;(containers:enqueue
-                                        ; input-queue
-                                        ; (make-instance 'key-event :host (hostname) :type :key-down :key k
-                                        ;                :mod m :mod-key m-k :unicode u))
-                             )
-            (:key-up-event (:key k :mod m :mod-key m-k :unicode u)
-                                        ;(containers:enqueue
-                                        ; input-queue
-                                        ; (make-instance 'key-event :host (hostname) :type :key-up :key k
-                                        ;                :mod m :mod-key m-k :unicode u))
-                           )
-            (:idle ()
-                ; this whole block is supposed to be run once per frame
-              (progn
-                ; move camera based on keyboard/xbox controller
-                
-                 (let* ((move-x (input-move-x *input*))
-                        (move-y (input-move-y *input*))
-                        (rot-amt  (* -1 move-x))
-                        (step-amt (*  1 move-y)))
+           (let ((input-queue (make-instance 'containers:basic-queue)))
+             (catch 'main-loop
+               (sdl:with-events ()
+                 (:quit-event () t)
+                 (:key-down-event (:key k :mod m :mod-key m-k :unicode u)
+                   (when (sdl:key= k :sdl-key-return)
+                     (if (eql (input-kind *input*) :keyboard)
+                         (set-controller *input* :xbox)
+                         (set-controller *input* :keyboard)))
+                   ;;(containers:enqueue
+                   ;; input-queue
+                   ;; (make-instance 'key-event :host (hostname) :type :key-down :key k
+                   ;;                :mod m :mod-key m-k :unicode u))
+                   )
+                 (:key-up-event (:key k :mod m :mod-key m-k :unicode u)
+                   ;;(containers:enqueue
+                   ;; input-queue
+                   ;; (make-instance 'key-event :host (hostname) :type :key-up :key k
+                   ;;                :mod m :mod-key m-k :unicode u))
+                   )
+                 (:idle ()
+                   ;; this whole block is supposed to be run once per frame
+
+                   ;; move camera based on keyboard/xbox controller
+                   (let* ((move-x (input-move-x *input*))
+                          (move-y (input-move-y *input*))
+                          (rot-amt  (* -1 move-x))
+                          (step-amt (*  1 move-y)))
                        
-                    (send-string :server (format nil "move-x: ~a~%" move-x))
-                    (send-string :server (format nil "move-y: ~a~%" move-y))
+                     (send-string :server (format nil "move-x: ~a~%" move-x))
+                     (send-string :server (format nil "move-y: ~a~%" move-y))
                      
-                     (setf (blt3d-gfx:cam-dir blt3d-gfx:*main-cam*) (quat-rotate-vec
-                        (axis-rad->quat (make-vec3 0.0 1.0 0.0) (deg->rad (* 2.7 rot-amt)))
-                        (blt3d-gfx:cam-dir blt3d-gfx:*main-cam*)))
+                     (setf (blt3d-gfx:cam-dir blt3d-gfx:*main-cam*)
+                           (quat-rotate-vec
+                            (axis-rad->quat (make-vec3 0.0 1.0 0.0)
+                                            (deg->rad (* 2.7 rot-amt)))
+                            (blt3d-gfx:cam-dir blt3d-gfx:*main-cam*)))
                      (setf (blt3d-gfx:cam-pos blt3d-gfx:*main-cam*) 
                            (vec4+ (blt3d-gfx:cam-pos blt3d-gfx:*main-cam*) 
-                                  (vec-scale4 (blt3d-gfx:cam-dir blt3d-gfx:*main-cam*) step-amt)) )
-                     )
-                         
-                (blt3d-gfx:render-frame nil)
-                
-                (socket-receive-all *my-client-buffer* #'handle-message-client 
-                    :timeout 0)
-                (send-string :server (format nil "Msg #~a (rand: ~a)" counter (random 10)))
-                (incf counter)
-                    
-            ))))))))
-)
+                                  (vec-scale4
+                                   (blt3d-gfx:cam-dir blt3d-gfx:*main-cam*)
+                                   step-amt))))
+
+                   (blt3d-gfx:render-frame nil)
+
+                   (socket-receive-all *my-client-buffer*
+                                       #'handle-message-client
+                                       :timeout 0)
+                   (send-string
+                    :server
+                    (format nil "Msg #~a (rand: ~a)" counter (random 10)))
+                   (incf counter)))))))))

9844a5bed32a6009a758768f66fedb43cbb2e0a2
Chris <askgeek@gmail.com>
2011-05-06 17:26:54 -0700

    Simple n^2 check setup

---
+++ src/blackthorn3d/import/package.lisp
@@ -0,0 +1,72 @@
+;;;; Blackthorn -- Lisp Game Engine
+;;;;
+;;;; Copyright (c) 2011, Robert Gross <r.gross.3@gmail.com>
+;;;;
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation
+;;;; files (the "Software"), to deal in the Software without
+;;;; restriction, including without limitation the rights to use, copy,
+;;;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;;;; of the Software, and to permit persons to whom the Software is
+;;;; furnished to do so, subject to the following conditions:
+;;;;
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;;; DEALINGS IN THE SOFTWARE.
+;;;;
+
+(in-package :cl-user)
+
+(defpackage :blackthorn3d-import
+  (:nicknames :blt3d-imp)
+  (:use :iter :cl :blt3d-utils :blt3d-math)
+  (:export
+
+   ;; dae-loader.lisp
+   :load-dae
+
+   ;; load-object.lisp
+   :load-object
+   :lo-meshes
+
+   :blt-mesh
+   :id
+   :vertex-streams
+   :elements
+   :controller
+   :transform
+   :vertices
+
+   :elem
+   :elem-indices
+   :elem-material
+
+   :model-node
+   :mesh
+   :transform
+   :material-array
+   :bounding-volume
+   :child-nodes
+
+   :vertex-stream
+   :vs-semantic
+   :vs-stride
+   :vs-stream
+
+   :blt-material
+   :ambient
+   :diffuse
+   :specular
+   :shininess
+
+   :organize-streams
+   :interleave
+   ))

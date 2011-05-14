44ca8fc891a5006cf0c7c9c2b2235a85430586cd
Chris <askgeek@gmail.com>
2011-05-14 16:21:38 -0700

    start of physics step

---
+++ src/blackthorn3d/math/vector.lisp
@@ -0,0 +1,240 @@
+;;;; Blackthorn -- Lisp Game Engine
+;;;;
+;;;; Copyright (c) 2011, Robert Gross <r.gross.3@gmail.com>
+;;;;
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation
+;;;; files (the "Software"), to deal in the Software without
+;;;; restriction, including without limitation the rights to use, copy,
+;;;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;;;; of the Software, and to permit persons to whom the Software is
+;;;; furnished to do so, subject to the following conditions:
+;;;;
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;;; DEALINGS IN THE SOFTWARE.
+;;;;
+
+(in-package :blackthorn3d-math)
+
+;;;;
+;;;; Vector Stuff
+;;;;
+
+(defmacro gen-vec-accessors (&rest names)
+  (labels ((vec-accessor (n p)
+             (with-gensyms (v) `(defmacro ,n (,v) `(svref ,,v ,,p)))))
+    `(progn
+       ,@(iter (for i in names)
+               (for j below (length names))
+               (collect (vec-accessor i j))))))
+        
+;;; Create aliases for accessing different elements
+;;; of vectors
+(gen-vec-accessors x y z w)
+(gen-vec-accessors r g b a)
+
+(defun make-color (r g b &optional (a 1.0))
+  "@short{Create a vector4 for color}
+   @arg[r]{red component, float [0 1]}
+   @arg[g]{green component, float [0 1]}
+   @arg[b]{blue component, float [0 1]}
+   @arg[a]{optional alpha defaults to 1}"
+  (make-vector4 r g b a))
+
+(defun make-vector4 (x y z w)
+  "@short{Creates a vector of length 4 of floats}
+   @arg[x]{element at index 0}
+   @arg[y]{element at index 1}
+   @arg[z]{element at index 2}
+   @arg[w]{element at index 3}"
+  (make-array 4 :element-type 'float :initial-contents
+              (list (float x) (float y) (float z) (float w))))
+
+
+(defun make-vector3 (x y z)
+  "@short{Creates a vector of length 3 of floats}
+   @arg[x]{element at index 0}
+   @arg[y]{element at index 1}
+   @arg[z]{element at index 2}"
+  (make-array 3 :element-type 'float :initial-contents
+    (list x y z)))
+
+(defun make-point3 (x y z)
+  "@short{Create a point in 3d space; automatically sets w to 1.0}"
+  (make-vector4 x y z 1.0))
+
+(defun make-vec3 (x y z)
+  "@short{Create a vector in 3d space; sets w to 0.0}"
+  (make-vector4 x y z 0.0))
+
+(defun dot (a b)
+  (iter (for i from 0 below 3)
+        (sum (* (aref a i) (aref b i)))))
+
+(defun cross (a b)
+  (make-vector4
+    (- (* (y a) (z b)) (* (z a) (y b)))
+    (- (* (z a) (x b)) (* (x a) (z b)))
+    (- (* (x a) (y b)) (* (y a) (x b)))
+    (w a)))
+
+(defun cross3 (a b)
+  (make-vector3
+    (- (* (y a) (z b)) (* (z a) (y b)))
+    (- (* (z a) (x b)) (* (x a) (z b)))
+    (- (* (x a) (y b)) (* (y a) (x b)))))
+
+(defmacro vector-elt-wise (fn a b)
+  `(map 'vector ,fn ,a ,b))
+
+(defmacro vec4-3elt-op (fn a b)
+  (with-gensyms (v1 v2)
+    (let ((v1 a) (v2 b))
+      `(make-vector4
+        (,fn (x ,v1) (x ,v2))
+        (,fn (y ,v1) (y ,v2))
+        (,fn (z ,v1) (z ,v2))
+        (w ,v1)))))
+
+(defmacro vec3-3elt-op (fn a b)
+  (with-gensyms (v1 v2)
+    (let ((v1 a) (v2 b))
+      `(make-vector3
+        (,fn (x ,v1) (x ,v2))
+        (,fn (y ,v1) (y ,v2))
+        (,fn (z ,v1) (z ,v2))))))
+
+(defun vec4+ (a b)
+  (vec4-3elt-op + a b))
+
+(defun vec3+ (a b)
+  (vec3-3elt-op + a b))
+  
+(defun vec4- (a b)
+  (vec4-3elt-op - a b))
+
+(defun vec3- (a b)
+  (vec3-3elt-op - a b))
+
+(defun vec4* (a b)
+  (vec4-3elt-op * a b))
+
+(defun vec3* (a b)
+  (vec3-3elt-op * a b))
+
+(defun vec4/ (a b)
+  (vec4-3elt-op / a b))
+
+(defun vec3/ (a b)
+  (vec3-3elt-op / a b))
+
+(defun vec-scale4 (v s)
+  (make-vector4 
+    (* s (x v))
+    (* s (y v))
+    (* s (z v))
+    (w v)))
+
+(defun vec-scale3 (v s)
+  (make-vector3
+    (* s (x v))
+    (* s (y v))
+    (* s (z v))))
+
+(defun vec-scale (v s)
+  (make-array (length v)
+              :initial-contents
+              (iter (for e in-vector v)
+                    (* e s))))
+
+(defun vec-neg4 (v)
+  (make-vector4 
+    (- (x v))
+    (- (y v))
+    (- (z v))
+    (w v)))
+
+(defun vec-neg3 (v)
+  (make-vector3 
+    (- (x v))
+    (- (y v))
+    (- (z v))))
+
+(defun sq (x) (* x x))
+
+(defun mag (v)
+  (sqrt (+ (+ (sq (x v))
+              (sq (y v)))
+           (sq (z v)))))
+
+(defun sq-mag (v)
+  (+ (+ (sq (x v))
+        (sq (y v))
+     (sq (z v)))))
+
+(defun norm4 (v)
+  "Normalize a vector4, see normalize for general normalize fn"
+  (let ((magv (mag v)))
+    (if (zerop magv) v
+        (vec-scale4 v (/ magv)))))
+
+(defun norm3 (v)
+  "Normalize a vector3."
+  (let ((magv (mag v)))
+    (if (zerop magv) v
+        (vec-scale3 v (/ magv)))))
+
+(defun normalize (v)
+  "Normalize a vector of any length"
+  (let ((magv (magnitude v)))
+    (unless (zerop magv)
+      (map 'vector #'(lambda (x) (/ x magv)) v))))
+
+(defun homogenize (v)
+  (unless (zerop (w v)) (vec-scale4 v (/ (w v)))))
+
+(defun min-axis (v)
+  (iter (for i below 4)
+        (finding i minimizing (svref v i))))
+
+(defun make-perp (v)
+  (let ((min-a (min-axis v)))
+    (case min-a
+      (0 (norm (make-vec3 0.0 (-(z v)) (y v))))
+      (1 (norm (make-vec3 (-(z v)) 0.0 (x v))))
+      (2 (norm (make-vec3 (-(y v)) (x v) 0.0))))))
+
+(defun spherical->cartesian (sphere-coords &optional vec-p)
+  (let* ((phi   (elt sphere-coords 0))
+         (theta (elt sphere-coords 1))
+         (r     (elt sphere-coords 2))
+         (sin-theta (sin (- (/ pi 2) theta))))
+    (make-vector4 (* r (sin phi) sin-theta)
+                  (* r (cos (- (/ pi 2) theta)))
+                  (* r (cos phi) sin-theta)
+                  (if vec-p 0.0 1.0))))
+
+
+(defun tri-centroid (v0 v1 v2)
+  (make-vector3 
+   (/ (+ (x v0) (x v1) (x v2)) 3.0)
+   (/ (+ (y v0) (y v1) (y v2)) 3.0)
+   (/ (+ (z v0) (z v1) (z v2)) 3.0)))
+
+;; TODO: fix this later
+#+disabled 
+(defun centroid (&rest vecs)
+  (let ((v-len (length (car vecs))))
+    (iter (for v in vecs)
+          (counting t into len)
+          (iter (for i below v-len)
+                (collect )))))
---
+++ src/blackthorn3d/physics/static-collision.lisp
@@ -0,0 +1,125 @@
+;;;; Blackthorn -- Lisp Game Engine
+;;;;
+;;;; Copyright (c) 2011, Robert Gross <r.gross.3@gmail.com>
+;;;;
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation
+;;;; files (the "Software"), to deal in the Software without
+;;;; restriction, including without limitation the rights to use, copy,
+;;;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;;;; of the Software, and to permit persons to whom the Software is
+;;;; furnished to do so, subject to the following conditions:
+;;;;
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;;; DEALINGS IN THE SOFTWARE.
+;;;;
+
+(in-package :blackthorn3d-physics)
+
+;;;
+;;; Collection of tools for static collision detection
+;;; This should be put somewhere else...
+;;;
+
+
+(defun make-rect (shape)
+  (destructuring-bind (lows-v highs-v) (shape-bounds shape)
+    (rectangles:make-rectangle
+     :lows (list (x lows-v) (y lows-v) (z lows-v))
+     :highs (list (x highs-v) (y highs-v) (z highs-v)))))
+
+(defun build-r-tree (triangles)
+  "take a vector of triangles and return an r-tree around them"
+  (let ((r-tree 
+         (spatial-trees:make-spatial-tree :r 
+                                          :rectfun #'make-rect)))
+    (iter (for tri in-vector triangles)
+          (spatial-trees:insert tri r-tree))
+    r-tree))
+
+;; For now, we'll just return t or nil
+;; probably will want the hit location ... 
+(defun sphere-rtree-intersection (sphere rtree)
+  ;; get the list of potentially intersecting triangles
+  (iter (for tri in (spatial-trees:search sphere rtree))
+        (collect (sphere-triangle-intersection sphere tri) into results)
+        (finally 
+         (return (iter (with min-d = `(1.0e+INF nil))
+                       (for hit in results)
+                       (unless (null hit)
+                         (when (< (car hit) (car min-d))
+                           (setf min-d hit)))
+                       (finally (return min-d)))))))
+
+(defun initialize-cube (level-model)
+  "Take a model file contains level information 
+  (a 30x30x30 cube) and create a thinger out of it.
+  r-tree...thinger...replace the blt-meshes with r-trees"
+  (labels ((helper (node)
+             (setf (mesh node)
+                   (build-r-tree (build-triangle-array (mesh node))))
+             (iter (for n in (child-nodes node))
+                   (helper n)))))
+  (iter (for node in (mesh-nodes level-model))
+        (helper node))
+  level-model)
+
+(defun min-collide (hits)
+  "Return the hit with the smallest distance from the collider"
+  (when hits
+    (iter (with min-d = '(1.0e+INF nil))
+          (for hit in hits)
+          (when (< (car hit) (car min-d))
+            (setf min-d hit))
+          (finally (return min-d)))))
+
+;(defgeneric collide-test (bv bv))
+(defmethod collide-test ((bv bounding-shape) (model blt-model))
+  "returns a point p if bv intersects with the geometry of model
+   nil otherwise"
+  (min-collide
+   (iter (for node in (mesh-nodes model))
+         (collect (collide-test bv node)))))
+
+(defmethod collide-test ((bv bounding-shape) (node model-node))
+  "recursively tests bv against this node and its children.  Note
+   that, since currently the bounding volume of a node is not 
+   guaranteed to enclose its children, the children must always be
+   tested"
+  ;; We need to translate, and transform, the collidee to the 
+  ;; node coordinates.  We do this instead of translating the 
+  ;; node b/c it's a lot easier to translate a bounding sphere than
+  ;; an r-tree.
+  (let ((xformed-bv (transform-bounding-volume 
+                     bv 
+                     (rt-inverse (transform node)))))
+    (min-collide
+     (cons (aif (collide-test xformed-bv (node-bounding-shape node))
+                ;; if we intersect the bounding-shape, check the mesh
+                (collide-test xformed-bv (mesh node)))
+           (iter (for child in (child-nodes node))
+                 (collect (collide-test xformed-bv child)))))))
+
+(defmethod collide-test ((bv bounding-shape) (mesh blt-mesh))
+  t)
+
+(defmethod collide-test ((sphere bounding-sphere) 
+                         (r-tree spatial-trees-protocol:spatial-tree))
+  (iter (for tri in (spatial-trees:search sphere rtree))
+        (collect (sphere-triangle-intersection sphere tri) into results)
+        (finally 
+         (return (iter (with min-d = `(1.0e+INF nil))
+                       (for hit in results)
+                       (unless (null hit)
+                         (when (< (car hit) (car min-d))
+                           (setf min-d hit)))
+                       (finally (return min-d)))))))

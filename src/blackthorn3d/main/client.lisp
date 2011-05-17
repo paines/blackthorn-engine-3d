99e187bf8b0219f66e6ee05a4d2f2d26a48c16ee
Robert Gross <r.gross.3@gmail.com>
2011-05-17 10:38:34 -0700

    added cylinder

--- src/blackthorn3d/main/client.lisp
+++ src/blackthorn3d/main/client.lisp
@@ -94,6 +94,10 @@
 
   ;; need to scale robot =(
   (blt3d-phy:apply-transform (get-model :wedge) (make-scale #(0.05 0.05 0.05)))
+  (blt3d-phy:apply-transform (get-model :wedge) 
+                             (make-inv-ortho-basis (make-point3 1.0 0.0 0.0)
+                                                   (make-point3 0.0 0.0 1.0)
+                                                   (make-point3 0.0 1.0 0.0)))
   
   (setf *level* (blt3d-gfx:load-obj->models 
                  (blt3d-imp:load-dae #p"res/models/test-room.dae")))

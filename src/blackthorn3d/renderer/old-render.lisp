becae958c8300461748a365f7a84297a642db2b1
Robert Gross <r.gross.3@gmail.com>
2011-05-31 15:25:29 -0700

    fix for ogl 2.0 (real)

--- src/blackthorn3d/renderer/old-render.lisp
+++ src/blackthorn3d/renderer/old-render.lisp
@@ -51,7 +51,7 @@
   
   ;; create shadow map
   ;#+disabled
-  (when (and *main-cam* (< *gl-version* 3.0))
+  (when (and *main-cam* (> *gl-version* 3.0))
     (shadow-pass *main-light* 
                  (cons home-sector 
                    ;(cons *test-skele*)

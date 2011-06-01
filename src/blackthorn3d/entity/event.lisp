4a5254115fb1b433f9c3222b2c7fe2a2fccdd902
Robert Gross <r.gross.3@gmail.com>
2011-05-31 19:22:07 -0700

    camera fix

---
+++ src/blackthorn3d/entity/event.lisp
@@ -0,0 +1,66 @@
+;;;; Blackthorn -- Lisp Game Engine
+;;;;
+;;;; Copyright (c) 2011, Elliott Slaughter <elliottslaughter@gmail.com>
+;;;;
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation
+;;;; files (the "Software"), to deal in the Software without
+;;;; restriction, including without limitation the rights to use, copy,
+;;;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;;;; of the Software, and to permit persons to whom the Software is
+;;;; furnished to do so, subject to the following conditions:
+;;;;
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;;; DEALINGS IN THE SOFTWARE.
+;;;;
+
+(in-package :blackthorn3d-entity)
+
+(make-list-serializer :entity-create-list :entity-create)
+(make-list-serializer :entity-update-list :entity-update)
+(make-list-serializer :entity-remove-list :entity-remove)
+
+(defmessage :event-entity-create nil (:entity-create-list))
+(defmessage :event-entity-update nil (:entity-update-list))
+(defmessage :event-entity-remove nil (:entity-remove-list))
+
+(defgeneric make-event (type &key))
+
+(defmethod make-event ((type (eql :entity-create)) &key include-all)
+  (let ((entities
+         (if include-all
+             (iter (for (nil entity) in-hashtable *global-oid-table*)
+                   (collect entity))
+             *recently-created-server-entities*)))
+    (make-message-list :event-entity-create entities)))
+
+(defmethod make-event ((type (eql :entity-update)) &key)
+  (let ((entities
+         (iter (for (nil entity) in-hashtable *global-oid-table*)
+               (with-slots (modified) entity
+                 (when modified (collect entity))))))
+    (make-message-list :event-entity-update entities)))
+
+(defmethod make-event ((type (eql :entity-remove)) &key)
+  (let ((entities *recently-removed-server-entities*))
+    (make-message-list :event-entity-remove entities)))
+
+(defmessage :event-input send-input
+  (:single-float ; move-x
+   :single-float ; move-y 
+   :single-float ; view-x
+   :single-float ; view-y
+   :single-float ; jmp
+   ))
+
+(defmessage :event-camera send-camera
+  (:entity-oid))
---
+++ src/blackthorn3d/sound/sound.lisp
@@ -0,0 +1,80 @@
+;;;; Blackthorn -- Lisp Game Engine
+;;;;
+;;;; Copyright (c) 2011, Elliott Slaughter <elliottslaughter@gmail.com>
+;;;;
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation
+;;;; files (the "Software"), to deal in the Software without
+;;;; restriction, including without limitation the rights to use, copy,
+;;;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;;;; of the Software, and to permit persons to whom the Software is
+;;;; furnished to do so, subject to the following conditions:
+;;;;
+;;;; The above copyright notice and this permission notice shall be
+;;;; included in all copies or substantial portions of the Software.
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;;; NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;;; DEALINGS IN THE SOFTWARE.
+;;;;
+
+(in-package :blackthorn3d-sound)
+
+(defun init ()
+  (sdl-mixer:open-audio))
+
+(defun init-p ()
+  (let ((open (sdl-mixer:audio-opened-p)))
+    (and open (> open 0))))
+
+(defun exit ()
+  ;;(sdl-mixer:halt-music)
+  (sdl-mixer:close-audio))
+
+(defclass sound ()
+  ((src
+    :initarg :src)
+   (sound
+    :accessor sound
+    :initarg :sound)))
+
+(defclass music (sound) ())
+
+(defclass sample (sound) ())
+
+(defgeneric load-sound (type src))
+
+(defmethod load-sound ((type (eql :music)) src)
+  (when (init-p)
+    (let ((sound (sdl-mixer:load-music (resolve-resource src))))
+      (make-instance 'music :src src :sound sound))))
+
+(defmethod load-sound ((type (eql :sample)) src)
+  (when (init-p)
+    (let ((sound (sdl-mixer:load-sample (resolve-resource src))))
+      (make-instance 'sample :src src :sound sound))))
+
+(defgeneric play-sound (sound &key))
+
+(defmethod play-sound ((music music) &key loop fade (position 0))
+  (when (init-p)
+    (sdl-mixer:play-music (sound music)
+                          :loop loop :fade fade :position position)))
+
+(defmethod play-sound ((sample sample) &key loop fade (ticks t))
+  (when (init-p)
+    (sdl-mixer:play-sample (sound sample)
+                           :loop loop :fade fade :ticks ticks)))
+
+(defgeneric stop-sound (type &key))
+
+(defmethod stop-sound ((type (eql :music)) &key)
+  (sdl-mixer:halt-music))
+
+(defmethod stop-sound ((type (eql :sample)) &key channel fade ticks)
+  (sdl-mixer:halt-sample channel :fade fade :ticks ticks))

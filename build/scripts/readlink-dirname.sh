b220dec694e34f2c80ba432b1c34a113b299c7e8
Elliott Slaughter <elliottslaughter@gmail.com>
2011-06-17 20:27:39 -0700

    Fix path handling which caused Mercurial download extension to not load.

--- build/scripts/readlink-dirname.sh
+++ build/scripts/readlink-dirname.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-cd "$(dirname "$0")" && pwd -P
+set -e
+
+cd "$(dirname "$0")"
+path="$(pwd -P)"
+if [[ -n $(uname -s | grep "MINGW") ]]; then
+    path="$(pwd -W)"
+elif [[ -n $(uname -s | grep "CYGWIN") ]]; then
+    path="$(cygpath -w "$path")"
+fi
+echo "$path"

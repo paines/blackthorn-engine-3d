19ea76e45571a6621e3bae1449d28bdeb8daee9d
Elliott Slaughter <elliottslaughter@gmail.com>
2011-06-26 14:34:41 -0700

    Refactor whitespace fixing to convert to unix format.

--- build/scripts/fix-whitespace.sh
+++ build/scripts/fix-whitespace.sh
@@ -2,4 +2,12 @@
 
 repo_dir="$(dirname "$(dirname "$("$(dirname "$BASH_SOURCE")/readlink-dirname.sh" "$BASH_SOURCE")")")"
 
-find "$repo_dir/src" -name '*.lisp' | while read f; do sed 's/[ 	]*$//' $f > $f.2 && mv $f.2 $f; done
+# fix-file <filename>
+function fix-file () {
+    file="$1"
+    dos2unix -q "$file" &&
+    sed 's/[ 	]*$//' "$file" > "$file.2" &&
+    mv "$file.2" "$file"
+}
+
+find "$repo_dir/src" -name '*.lisp' | while read f; do fix-file $f; done
